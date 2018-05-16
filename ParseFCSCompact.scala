9d25f1a47d0b6b907c4c77f60d6b73bad6dc0535
gautier Mac <gautier.stoll@gmail.com>
2018-05-16 13:39:13 +0200

    With plotting object

--- ParseFCSCompact.scala
+++ ParseFCSCompact.scala
@@ -1,83 +1,42 @@
 
-class FCSParserCompact(fcsNameInput: String, minValCytInput : Double) {
+import java.io._
+import breeze.linalg._
+import breeze.numerics._
+import java.nio.ByteBuffer
+import org.saddle._
+import stat._
+import scala.util._
+import org.nspl._
+import org.nspl.saddle._
+import org.nspl.data._
+import org.nspl.awtrenderer._
+import org.saddle.io._
+import stat.kmeans._
+import stat.sparse.SMat
 
-  import java.io._
-  //import java.nio.file.{Files, Paths}
-  //import scala.util.matching.Regex
-  import breeze.linalg._
-  import breeze.numerics._
-  import java.nio.ByteBuffer
-  import org.saddle._
-  import stat._
-  import scala.util._
-  //import scala.math._
-  //import org.nspl.{DiscreteColors, TableLayout, point,sequence, xyplot,Plots,PointLegend,Shape}
-  import org.nspl._
-  import org.nspl.saddle._
-  import org.nspl.data._
-  import org.nspl.awtrenderer._
-  import org.saddle.io._
-  import stat.kmeans.KMeansResult
-  import stat.sparse.SMat
+class FCSParserCompact(fcsNameInput: String, minValCytInput: Double) {
 
-  private val offsetByteText : (Int,Int,Int) = (10,17,25)
-  private val offsetByteAnalysis : (Int,Int,Int) = (42,49,57)
-
-  val fcsFile = new String(fcsNameInput)
-  val minValCyt = minValCytInput
-
-  private val fcsFileBuffer = new BufferedInputStream(new FileInputStream(fcsFile))
-
-  private var binaryFileIndex: Int = 0
-  for (i <- (0 to (offsetByteText._1-1))) {
-    fcsFileBuffer.read
-    binaryFileIndex +=1
-  }
-  private val firstTextSegment = (for (i <- (offsetByteText._1 to offsetByteText._2)) yield {
-    fcsFileBuffer.read
-  }).
-    map(_.toChar).filter(_ != ' ').mkString("").toInt;
-  binaryFileIndex = offsetByteText._2+1
-
-  private val lastTextSegment = (for (i <- (binaryFileIndex to offsetByteText._3)) yield {
-    fcsFileBuffer.read
-  }).
-    map(_.toChar).filter(_ != ' ').mkString("").toInt
-  binaryFileIndex = offsetByteText._3+1
-  for (i <- (binaryFileIndex to (offsetByteAnalysis._1-1))) yield {
-    fcsFileBuffer.read
-    binaryFileIndex +=1
-  }
-  private val firstAnalysisSegment = (for (i <- (binaryFileIndex to offsetByteAnalysis._2)) yield {
-    fcsFileBuffer.read
-  }).
-    map(_.toChar).filter(_ != ' ').mkString("").toInt
-  binaryFileIndex = offsetByteAnalysis._2+1
-
-  private val LastAnalysisSegment = (for (i <- (binaryFileIndex to offsetByteAnalysis._3)) yield {
-    fcsFileBuffer.read
-  }).
-    map(_.toChar).filter(_ != ' ').mkString("").toInt;
-  binaryFileIndex = offsetByteAnalysis._3+1;
-
-  private def lengthSecondCharSep(inList: List[Byte]): Int = {
-    def dropUntilSinglSep(ByteSeparator: Byte, offsetcharList: Int, charList: List[Byte]): Int = {
-      var newOffsetcharList = offsetcharList + charList.drop(offsetcharList).takeWhile(_ != ByteSeparator).length
-      charList.drop(newOffsetcharList) match {
-        case ByteSeparator :: Nil => newOffsetcharList // two separators is not a separator
-        case ByteSeparator :: ByteSeparator :: yy => dropUntilSinglSep(ByteSeparator, newOffsetcharList + 2, charList)
-        case ByteSeparator :: yy => newOffsetcharList
-        case yy => {
-          sys.error("Error in Parsing text segment");
-          0
-        }
-      }
-    }
-
-    dropUntilSinglSep(inList.head, 1, inList)
-  }
+  private val offsetByteText: (Int, Int, Int) = (10, 17, 25)
+  private val offsetByteAnalysis: (Int, Int, Int) = (42, 49, 57)
 
   private def textSegmentMap(inList: List[Byte]): Map[String, String] = {
+    def lengthSecondCharSep(inList: List[Byte]): Int = {
+      def dropUntilSinglSep(ByteSeparator: Byte, offsetcharList: Int, charList: List[Byte]): Int = {
+        var newOffsetcharList = offsetcharList + charList.drop(offsetcharList).takeWhile(_ != ByteSeparator).length
+        charList.drop(newOffsetcharList) match {
+          case ByteSeparator :: Nil => newOffsetcharList // two separators is not a separator
+          case ByteSeparator :: ByteSeparator :: yy => dropUntilSinglSep(ByteSeparator, newOffsetcharList + 2, charList)
+          case ByteSeparator :: yy => newOffsetcharList
+          case yy => {
+            sys.error("Error in Parsing text segment");
+            0
+          }
+        }
+      }
+
+      dropUntilSinglSep(inList.head, 1, inList)
+    }
+
     val keyLength = lengthSecondCharSep(inList) - 1
     val valLength = lengthSecondCharSep(inList.drop(keyLength + 1)) - 1
     if (inList.length <= (keyLength + valLength + 3)) {
@@ -91,6 +50,44 @@ class FCSParserCompact(fcsNameInput: String, minValCytInput : Double) {
     }
   }
 
+
+  val fcsFile = new String(fcsNameInput)
+  val minValCyt = minValCytInput
+
+  private val fcsFileBuffer = new BufferedInputStream(new FileInputStream(fcsFile))
+
+  private var binaryFileIndex: Int = 0
+  for (i <- (0 to (offsetByteText._1 - 1))) {
+    fcsFileBuffer.read
+    binaryFileIndex += 1
+  }
+  private val firstTextSegment = (for (i <- (offsetByteText._1 to offsetByteText._2)) yield {
+    fcsFileBuffer.read
+  }).
+    map(_.toChar).filter(_ != ' ').mkString("").toInt;
+  binaryFileIndex = offsetByteText._2 + 1
+
+  private val lastTextSegment = (for (i <- (binaryFileIndex to offsetByteText._3)) yield {
+    fcsFileBuffer.read
+  }).
+    map(_.toChar).filter(_ != ' ').mkString("").toInt
+  binaryFileIndex = offsetByteText._3 + 1
+  for (i <- (binaryFileIndex to (offsetByteAnalysis._1 - 1))) yield {
+    fcsFileBuffer.read
+    binaryFileIndex += 1
+  }
+  private val firstAnalysisSegment = (for (i <- (binaryFileIndex to offsetByteAnalysis._2)) yield {
+    fcsFileBuffer.read
+  }).
+    map(_.toChar).filter(_ != ' ').mkString("").toInt
+  binaryFileIndex = offsetByteAnalysis._2 + 1
+
+  private val LastAnalysisSegment = (for (i <- (binaryFileIndex to offsetByteAnalysis._3)) yield {
+    fcsFileBuffer.read
+  }).
+    map(_.toChar).filter(_ != ' ').mkString("").toInt;
+  binaryFileIndex = offsetByteAnalysis._3 + 1;
+
   for (i <- (binaryFileIndex to (firstTextSegment - 1))) yield {
     fcsFileBuffer.read
   };
@@ -100,12 +97,12 @@ class FCSParserCompact(fcsNameInput: String, minValCytInput : Double) {
     fcsFileBuffer.read
   }).map(_.toByte)
   binaryFileIndex = lastTextSegment + 1;
-   val fcsTextSegmentMap = textSegmentMap(fcsTextSegment.toList)
+  val fcsTextSegmentMap = textSegmentMap(fcsTextSegment.toList)
   println("Mode: " + fcsTextSegmentMap("$MODE"))
   println("Data type: " + fcsTextSegmentMap("$DATATYPE"))
   println("Number of chanels: " + fcsTextSegmentMap("$PAR"))
   println("Byte order: " + fcsTextSegmentMap("$BYTEORD"))
-  println("Number of events: "+fcsTextSegmentMap("$TOT"))
+  println("Number of events: " + fcsTextSegmentMap("$TOT"))
 
   private val firstDataSegment = fcsTextSegmentMap("$BEGINDATA").toList.filter(_ != ' ').mkString("").toInt
   private val lastDataSegment = fcsTextSegmentMap("$ENDDATA").toList.filter(_ != ' ').mkString("").toInt
@@ -123,7 +120,7 @@ class FCSParserCompact(fcsNameInput: String, minValCytInput : Double) {
 
   private var dataCompensatedArrayFCS = new Array[Double](nbEvent * compensatedParam.length)
   for (indexFCS <- (0 to (nbEvent * nbPar - 1))) {
-    print(indexFCS+"\r")
+    print(indexFCS + "\r")
     if (bitToFloat(indexFCS - (indexFCS / bitToFloat.length) * bitToFloat.length) == 32) {
       binaryFileIndex += 4
       val tempFileArray = ByteBuffer.wrap((1 to 4).map(x => fcsFileBuffer.read.toByte).toArray)
@@ -131,7 +128,7 @@ class FCSParserCompact(fcsNameInput: String, minValCytInput : Double) {
         val compArrayIndex: Int = (0 to (compensatedParam.length - 1)).
           filter(x => (compensatedParam(x) == (1 + indexFCS - (indexFCS / nbPar) * nbPar))).head +
           (indexFCS / nbPar) * compensatedParam.length
-        dataCompensatedArrayFCS(compArrayIndex) = log10(tempFileArray.getFloat.toDouble-minValCyt)
+        dataCompensatedArrayFCS(compArrayIndex) = log10(tempFileArray.getFloat.toDouble - minValCyt)
       }
     }
     if (bitToFloat(indexFCS - (indexFCS / bitToFloat.length) * bitToFloat.length) == 64) {
@@ -141,7 +138,7 @@ class FCSParserCompact(fcsNameInput: String, minValCytInput : Double) {
         val compArrayIndex = (0 to (compensatedParam.length - 1)).
           filter(x => (compensatedParam(x) == (1 + indexFCS - (indexFCS / nbPar) * nbPar))).head +
           (indexFCS / nbPar) * compensatedParam.length
-        dataCompensatedArrayFCS(compArrayIndex) = log10(tempFileArray.getDouble-minValCyt)
+        dataCompensatedArrayFCS(compArrayIndex) = log10(tempFileArray.getDouble - minValCyt)
       }
     }
   }
@@ -154,62 +151,100 @@ class FCSParserCompact(fcsNameInput: String, minValCytInput : Double) {
     }
     return (FCSMatrix)
   }
+//
+//
+//  def kmeanCompensatedSubPlot(nbRowsFCS: Int, sizeK: Int, it: Int, seed: Int, maxComb: Int, pngFile: String) = {
+//    val dataSubFCS = dataCompensatedMatFCS.row((0 to (nbRowsFCS - 1)).toArray)
+//    val rand4K = new Random(seed)
+//    val dataInitK = dataSubFCS.row((1 to sizeK).map(x => rand4K.nextInt(nbRowsFCS)).toArray)
+//    val resK = kmeans.apply(dataSubFCS, dataInitK, it)
+//    println(resK.means)
+//    //val dataMatrixSubFCS = kmeans.matToSparse(dataSubFCS)
+//    val projections = 0 until maxComb combinations (2) map { g =>
+//      val c1 = g(0)
+//      val c2 = g(1)
+//      val col1 = dataSubFCS.col(c1)
+//      val col2 = dataSubFCS.col(c2)
+//      xyplot(
+//        Mat(col1, col2, resK.clusters.map(_.toDouble)) -> point(
+//          labelText = false,
+//          color = DiscreteColors(resK.clusters.length)))(
+//        extraLegend = resK.clusters.toArray.distinct.map(
+//          x =>
+//            x.toString -> PointLegend(shape = Shape.rectangle(0, 0, 1, 1),
+//              color = DiscreteColors(resK.clusters.length)(x.toDouble))),
+//        xlab = compensatedParam.map(x => fcsTextSegmentMap("$P" + x + "S")).toList(c1),
+//        ylab = compensatedParam.map(x => fcsTextSegmentMap("$P" + x + "S")).toList(c2)
+//      )
+//    }
+//    val plot2DKMean = sequence(projections.toList, TableLayout(4))
+//    val pngFileKMean = new File(pngFile)
+//    pngToFile(pngFileKMean, plot2DKMean.build)
+//  }
+//
+//  def kmeanCompensatedSubPlotSeq(nbRowsFCS: Int, sizeK: Int, it: Int, seed: Int, maxComb: Int) = {
+//    val dataSubFCS = dataCompensatedMatFCS.row((0 to (nbRowsFCS - 1)).toArray)
+//    val rand4K = new Random(seed)
+//    val dataInitK = dataSubFCS.row((1 to sizeK).map(x => rand4K.nextInt(nbRowsFCS)).toArray)
+//    val resK = kmeans.apply(dataSubFCS, dataInitK, it)
+//    println(resK.means)
+//    //val dataMatrixSubFCS = kmeans.matToSparse(dataSubFCS)
+//    val projections = 0 until maxComb combinations (2) map { g =>
+//      val c1 = g(0)
+//      val c2 = g(1)
+//      val col1 = dataSubFCS.col(c1)
+//      val col2 = dataSubFCS.col(c2)
+//      xyplot(
+//        Mat(col1, col2, resK.clusters.map(_.toDouble)) -> point(
+//          labelText = false,
+//          color = DiscreteColors(resK.clusters.length)))(
+//        extraLegend = resK.clusters.toArray.distinct.map(
+//          x =>
+//            x.toString -> PointLegend(shape = Shape.rectangle(0, 0, 1, 1),
+//              color = DiscreteColors(resK.clusters.length)(x.toDouble))),
+//        xlab = compensatedParam.map(x => fcsTextSegmentMap("$P" + x + "S")).toList(c1),
+//        ylab = compensatedParam.map(x => fcsTextSegmentMap("$P" + x + "S")).toList(c2)
+//      )
+//    }
+//    sequence(projections.toList, TableLayout(4))
+//  }
 
-
-  def kmeanCompensatedSubPlot(nbRowsFCS: Int, sizeK: Int, it: Int, seed: Int, maxComb : Int,pngFile : String) = {
-    val dataSubFCS = dataCompensatedMatFCS.row((0 to (nbRowsFCS - 1)).toArray)
-    val rand4K = new Random(seed)
-    val dataInitK = dataSubFCS.row((1 to sizeK).map(x => rand4K.nextInt(nbRowsFCS)).toArray)
-    val resK = kmeans.apply(dataSubFCS, dataInitK, it)
-    println(resK.means)
-    //val dataMatrixSubFCS = kmeans.matToSparse(dataSubFCS)
-    val projections = 0 until maxComb combinations (2) map { g =>
-      val c1 = g(0)
-      val c2 = g(1)
-      val col1 = dataSubFCS.col(c1)
-      val col2 = dataSubFCS.col(c2)
-      xyplot(
-        Mat(col1, col2, resK.clusters.map(_.toDouble)) -> point(
-          labelText = false,
-          color = DiscreteColors(resK.clusters.length)))(
-        extraLegend = resK.clusters.toArray.distinct.map(
-          x =>
-            x.toString -> PointLegend(shape = Shape.rectangle(0, 0, 1, 1),
-              color = DiscreteColors(resK.clusters.length)(x.toDouble))),
-        xlab = compensatedParam.map(x=> fcsTextSegmentMap("$P" + x+ "S")).toList(c1),
-        ylab = compensatedParam.map(x=> fcsTextSegmentMap("$P" + x+ "S")).toList(c2)
-      )
-    }
-    val plot2DKMean = sequence(projections.toList, TableLayout(4))
-    val pngFileKMean = new File(pngFile)
-    pngToFile(pngFileKMean,plot2DKMean.build)
+  def kmeansCompensated(kMeanFCSInput: KMeanFCSInput): KMeansResult = {
+    val dataSubFCS = dataCompensatedMatFCS.row((0 to (kMeanFCSInput.nbRows - 1)).toArray)
+    val rand4K = new Random(kMeanFCSInput.seedK)
+    val dataInitK = dataSubFCS.row((1 to kMeanFCSInput.clusterNb).map(x => rand4K.nextInt(kMeanFCSInput.nbRows)).toArray)
+    kmeans.apply(dataSubFCS, dataInitK, kMeanFCSInput.iterations)
   }
+}
 
-  def kmeanCompensatedSubPlotSeq(nbRowsFCS: Int, sizeK: Int, it: Int, seed: Int, maxComb : Int) = {
-    val dataSubFCS = dataCompensatedMatFCS.row((0 to (nbRowsFCS - 1)).toArray)
-    val rand4K = new Random(seed)
-    val dataInitK = dataSubFCS.row((1 to sizeK).map(x => rand4K.nextInt(nbRowsFCS)).toArray)
-    val resK = kmeans.apply(dataSubFCS, dataInitK, it)
-    println(resK.means)
-    //val dataMatrixSubFCS = kmeans.matToSparse(dataSubFCS)
-    val projections = 0 until maxComb combinations (2) map { g =>
+case class KMeanFCSInput(clusterNb: Int = 5, nbRows: Int = 100, iterations: Int = 100, seedK: Int = 0) {}
+
+object FCSPlotting {
+  def kMeanFCSPlotSeq(fcsParsed: FCSParserCompact, kMeanR: KMeansResult)
+ : Build[ElemList[Elems2[XYPlotArea,Legend]]] = {
+    val dataSubFCS = fcsParsed.dataCompensatedMatFCS.row((0 to (kMeanR.clusters.length - 1)).toArray)
+    val projections = 0 until fcsParsed.compensatedParam.length combinations(2) map { g =>
       val c1 = g(0)
       val c2 = g(1)
       val col1 = dataSubFCS.col(c1)
       val col2 = dataSubFCS.col(c2)
       xyplot(
-        Mat(col1, col2, resK.clusters.map(_.toDouble)) -> point(
+        Mat(col1, col2, kMeanR.clusters.map(_.toDouble)) -> point(
           labelText = false,
-          color = DiscreteColors(resK.clusters.length)))(
-        extraLegend = resK.clusters.toArray.distinct.map(
+          color = DiscreteColors(kMeanR.clusters.length)))(
+        extraLegend = kMeanR.clusters.toArray.distinct.map(
           x =>
             x.toString -> PointLegend(shape = Shape.rectangle(0, 0, 1, 1),
-              color = DiscreteColors(resK.clusters.length)(x.toDouble))),
-        xlab = compensatedParam.map(x=> fcsTextSegmentMap("$P" + x+ "S")).toList(c1),
-        ylab = compensatedParam.map(x=> fcsTextSegmentMap("$P" + x+ "S")).toList(c2)
+              color = DiscreteColors(kMeanR.clusters.length)(x.toDouble))),
+        xlab = fcsParsed.compensatedParam.map(x => fcsParsed.fcsTextSegmentMap("$P" + x + "S")).toList(c1),
+        ylab = fcsParsed.compensatedParam.map(x => fcsParsed.fcsTextSegmentMap("$P" + x + "S")).toList(c2)
       )
     }
     sequence(projections.toList, TableLayout(4))
   }
-
+  def plotKSeqToPng(plotSeq : Build[ElemList[Elems2[org.nspl.XYPlotArea,org.nspl.Legend]]],
+                    fileName : String, widthPng : Int = 1000) = {
+    val filePng = new File(fileName)
+    pngToFile(filePng,plotSeq.build,widthPng)
+  }
 }
